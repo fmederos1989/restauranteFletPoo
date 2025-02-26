5aa8d048dbe7f549cc01ea416b1b434f49b0422e
Fede Mederos <fmederos1989@gmail.com>
2025-02-26 16:46:30 -0300

    visualizacion de mesas 2

--- app.py
+++ app.py
@@ -49,10 +49,24 @@ class RestauranteGUI:
             width=700,
             expand=True
         )
-        return ft.Container(
-            content=ft.Column([ft.Text('Mesas del Restaurante', size=20, weight=ft.FontWeight.BOLD),
+        return ft.Row(
+            controls=[
+                ft.Container(
+                    width=700,
+                    content=ft.Column([ft.Text('Mesas del Restaurante', size=20, weight=ft.FontWeight.BOLD),
                                self.grid_container
-        ])
+                              ], expand=True),
+                    expand=True
+                 ),
+                ft.VerticalDivider(),
+                ft.Container(
+                    width=400,
+                    content=self.crear_panel_gestion(),
+                    expand=True
+                )
+            ],
+            expand=True,
+            spacing=0
         )
 
     #! Métodos internos de vistas
@@ -96,15 +110,95 @@ class RestauranteGUI:
                     padding=5,
                     margin=5,
                     border_radius=5,
+                    ink=True,
+                    on_click= lambda e, num=mesa.numero: self.seleccionar_mesa(e, num)
                 )
             )
         return grid
+
 
     def seleccionar_mesa(self, e, numero_mesa):
         self.mesa_seleccionada = self.restaurante.buscar_mesa(numero_mesa)
         mesa = self.mesa_seleccionada
 
         #self.mesa_info.value = f'Mesa {mesa.numero} - Capacidad: {mesa.tamaño} personas'
+
+        e.page.update()
+        return None
+
+    def crear_panel_gestion(self):
+        self.mesa_seleccionada = None
+        self.mesa_info =ft.Text('', size=16,weight=ft.FontWeight.BOLD)
+        self.tamaño_grupo_input = ft.TextField(
+            label='Tamaño del Grupo',
+            input_filter=ft.NumbersOnlyInputFilter(),
+            prefix_icon=ft.Icons.PEOPLE
+        )
+
+        self.tipo_item_dropdown = ft.Dropdown(
+            label='Tipo de Item',
+            options=[
+                ft.dropdown.Option('Entrada'),
+                ft.dropdown.Option('Plato Principal'),
+                ft.dropdown.Option('Postre'),
+                ft.dropdown.Option('Bebida'),
+            ],
+            width=200,
+            on_change=self.actualizar_items_menu
+        )
+
+        self.items_dropdown = ft.Dropdown(
+            label='Seleccionar Item',
+            width=200,
+        )
+
+        self.asignar_btn = ft.ElevatedButton(
+            text='Asignar Cliente',
+            on_click=self.asignar_cliente,
+            disabled=True,
+            style=ft.ButtonStyle(
+                bgcolor=ft.Colors.GREEN_700,
+                color=ft.Colors.WHITE
+            )
+        )
+
+        return ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.Container(
+                        content=self.mesa_info,
+                        bgcolor=ft.Colors.BLUE_GREY_900,
+                        padding=10,
+                        border_radius=10,
+                    ),
+                    ft.Container(height=20),
+                    self.tamaño_grupo_input,
+                    self.tipo_item_dropdown,
+                    self.items_dropdown
+                ]
+            )
+        )
+
+    def actualizar_items_menu(self, e):
+        tipo = self.tipo_item_dropdown.value
+        self.items_dropdown.value = []
+
+        if tipo == 'Entrada':
+            items = self.restaurante.menu.entradas
+        elif tipo == 'Plato Principal':
+            items = self.restaurante.menu.plato_principal
+        elif tipo == 'Postre':
+            items = self.restaurante.menu.postres
+        elif tipo == 'Bebida':
+            items = self.restaurante.menu.bebidas
+        else:
+            items = []
+
+        self.items_dropdown.options = [
+            ft.dropdown.Option(item.nombre) for item in items
+        ]
+        if e and e.page:
+            e.page.update()
 
 def main():
     app = RestauranteGUI()
